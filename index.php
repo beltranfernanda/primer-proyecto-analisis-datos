1199d8c73a5ebca842c9c102b5c19f1b57b7ac67
jvoToryTech <james.valencia@tory-tech.com>
2022-06-14 21:40:11 -0500

    separacion de scripts y styles de index.php

--- index.php
+++ index.php
@@ -1,136 +1,35 @@
 <!DOCTYPE html>
 <html>
-<head>
+  <head>
+    <link rel="stylesheet" href="./public/css/style.css">
+  </head>
+  <body>
 
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
-<script src="https://code.highcharts.com/modules/export-data.js"></script>
-<script src="https://code.highcharts.com/modules/accessibility.js"></script>
+    <h1>Grupo 2</h1>
 
-<style>
-.highcharts-figure,
-.highcharts-data-table table {
-  min-width: 320px;
-  max-width: 660px;
-  margin: 1em auto;
-}
+    <?php
 
-.highcharts-data-table table {
-  font-family: Verdana, sans-serif;
-  border-collapse: collapse;
-  border: 1px solid #ebebeb;
-  margin: 10px auto;
-  text-align: center;
-  width: 100%;
-  max-width: 500px;
-}
+    echo "This is our testing server grupo 2<br>";
 
-.highcharts-data-table caption {
-  padding: 1em 0;
-  font-size: 1.2em;
-  color: #555;
-}
+    $data = file_get_contents("http://evalua.fernandoyepesc.com/04_Modules/11_Evalua/10_WS/ws_evitems.php?&eboxid=89");
 
-.highcharts-data-table th {
-  font-weight: 600;
-  padding: 0.5em;
-}
+    $json_data = json_decode($data, true);
+    print_r($json_data[0]['id']);
 
-.highcharts-data-table td,
-.highcharts-data-table th,
-.highcharts-data-table caption {
-  padding: 0.5em;
-}
+    ?>
+    <figure class="highcharts-figure">
+      <div id="container"></div>
+      
+      <p class="highcharts-description">
+        This pie chart shows how the chart legend can be used to provide
+        information about the individual slices.
+      </p>
+    </figure>
 
-.highcharts-data-table thead tr,
-.highcharts-data-table tr:nth-child(even) {
-  background: #f8f8f8;
-}
-
-.highcharts-data-table tr:hover {
-  background: #f1f7ff;
-}
-</style>
-</head>
-<body>
-
-<h1>Grupo 2</h1>
-
-<?php
-
-echo "This is our testing server grupo 2<br>";
-
-$data = file_get_contents("http://evalua.fernandoyepesc.com/04_Modules/11_Evalua/10_WS/ws_evitems.php?&eboxid=89");
-
-$json_data = json_decode($data, true);
-print_r($json_data[0]['id']);
-
-?>
-<figure class="highcharts-figure">
-  <div id="container"></div>
-  <p class="highcharts-description">
-    This pie chart shows how the chart legend can be used to provide
-    information about the individual slices.
-  </p>
-</figure>
-
-<script>
-	Highcharts.chart('container', {
-  chart: {
-    plotBackgroundColor: null,
-    plotBorderWidth: null,
-    plotShadow: false,
-    type: 'pie'
-  },
-  title: {
-    text: 'Browser market shares in January, 2018'
-  },
-  tooltip: {
-    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-  },
-  accessibility: {
-    point: {
-      valueSuffix: '%'
-    }
-  },
-  plotOptions: {
-    pie: {
-      allowPointSelect: true,
-      cursor: 'pointer',
-      dataLabels: {
-        enabled: false
-      },
-      showInLegend: true
-    }
-  },
-  series: [{
-    name: 'Brands',
-    colorByPoint: true,
-    data: [{
-      name: 'Chrome',
-      y: 61.41,
-      sliced: true,
-      selected: true
-    }, {
-      name: 'Internet Explorer',
-      y: 11.84
-    }, {
-      name: 'Firefox',
-      y: 10.85
-    }, {
-      name: 'Edge',
-      y: 4.67
-    }, {
-      name: 'Safari',
-      y: 4.18
-    }, {
-      name: 'Other',
-      y: 7.05
-    }]
-  }]
-});
-
-</script>
-
-</body>
+  </body>
+  <script src="https://code.highcharts.com/highcharts.js"></script>
+  <script src="https://code.highcharts.com/modules/exporting.js"></script>
+  <script src="https://code.highcharts.com/modules/export-data.js"></script>
+  <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+  <script src="./public/js/index.js"></script>
 </html>
