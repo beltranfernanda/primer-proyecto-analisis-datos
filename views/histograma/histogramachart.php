fa192193a4b74646d27aaae0d12c6b0535d798ca
lxahumadau <lauraahumada571@gmail.com>
2022-06-15 20:38:19 -0500

    modifed histogram

--- views/histograma/histogramachart.php
+++ views/histograma/histogramachart.php
@@ -1,7 +1,7 @@
 <div class="card">
     <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">Histograma</h5>
         <canvas id="histograma"></canvas>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <p class="card-text">Graph representing the age of the people</p>
     </div>
 </div>
