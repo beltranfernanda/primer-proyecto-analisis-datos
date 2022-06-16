bd0d60ebdce452180ed010079a88ca7f08784f30
Maria Fernanda Velandia <mfvelandiab@outlook.com>
2022-06-15 19:27:39 -0500

    Template added

--- index.php
+++ index.php
@@ -1,35 +1,61 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="./public/css/style.css">
   </head>
   <body>
-
-    <h1>Grupo 2</h1>
-
     <?php
-
-    echo "This is our testing server grupo 2<br>";
-
     $data = file_get_contents("http://evalua.fernandoyepesc.com/04_Modules/11_Evalua/10_WS/ws_evitems.php?&eboxid=89");
-
     $json_data = json_decode($data, true);
-    print_r($json_data[0]['id']);
-
+   // print_r($json_data[0]['id']);
     ?>
-    <figure class="highcharts-figure">
-      <div id="container"></div>
-      
-      <p class="highcharts-description">
-        This pie chart shows how the chart legend can be used to provide
-        information about the individual slices.
-      </p>
-    </figure>
-
+    <div class="container my-3">
+      <h1>Grupo 2</h1>
+    </div>
+    <div class="container my-3">
+      <h2>Charts</h2>
+      <!-- Inicio primera fila-->
+      <div class="row my-3">
+        <!-- Inicio primer columna-->
+        <div class="col">
+          <?php require 'views\histograma\histogramachart.php'; ?>
+        </div>
+        <!-- Inicio segunda columna-->
+        <div class="col">
+          <?php require 'views\lateralidad\lateralidadchart.php'; ?>
+        </div>
+      </div>
+      <!-- Inicio segunda fila-->
+      <div class="row my-3">
+        <!-- Inicio primer columna-->
+        <div class="col">
+          <?php require 'views\lateralidad\lateralidadchart.php'; ?>
+        </div>
+        <!-- Inicio segunda columna-->
+        <div class="col">
+          <?php require 'views\lateralidad\lateralidadchart.php'; ?>
+        </div>
+      </div>
+      <!-- Inicio tercera fila-->
+      <div class="row my-3">
+        <!-- Inicio primer columna-->
+        <div class="col">
+          <?php require 'views\lateralidad\lateralidadchart.php'; ?>
+        </div>
+        <!-- Inicio segunda columna-->
+        <div class="col">
+          <?php require 'views\lateralidad\lateralidadchart.php'; ?>
+        </div>
+      </div>
+    </div>
   </body>
-  <script src="https://code.highcharts.com/highcharts.js"></script>
-  <script src="https://code.highcharts.com/modules/exporting.js"></script>
-  <script src="https://code.highcharts.com/modules/export-data.js"></script>
-  <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+  <!--Bootstrap-->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  <!-- Chart js-->
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   <script src="./public/js/index.js"></script>
+  <script src="./public/js/lateralidad.js"></script>
+  <script src="./public/js/histograma.js"></script>
 </html>
