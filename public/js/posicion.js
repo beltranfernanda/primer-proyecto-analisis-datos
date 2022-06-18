c6790859349e0388de50759299370d797fe35701
sanlopezg <sanlopezg@correo.udistrital.edu.co>
2022-06-18 15:23:01 -0500

    grafica-pos-escolaridad

---
+++ public/js/posicion.js
@@ -0,0 +1,60 @@
+const objetoPosicion = responseService.map(item => item.valores[4])
+const posiciones = Object.values(objetoPosicion)
+
+var lista = [];
+var lista2 = [];
+var cantidades = [];
+var contador = 1;
+
+for (let i = 0; i <= posiciones.length; i++) {
+    if (posiciones[i] !== undefined) {
+        let pos = posiciones[i]['value'];
+        if (lista.includes(pos) === false) {
+            lista.push(pos)
+        }
+        pos = '';
+    }
+}
+
+for (let i = 0; i <= posiciones.length; i++) {
+    if (posiciones[i] !== undefined) {
+        let pos = posiciones[i]['value'];
+        pos.toUpperCase()
+        lista2.push(pos)
+        pos = '';
+    }
+}
+
+lista.sort()
+lista2.sort()
+for(let i = 0; i <= lista2.length; i++){
+    if(lista2[i + 1] === lista2[i]){
+        contador++;
+    } else {
+        cantidades.push(contador);
+        contador = 1;
+    }
+}
+
+const dataPosicion = {
+    labels: lista,
+    datasets: [{
+        label: 'Graficar Posición',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: cantidades,
+    }]
+};
+
+const configPosicion = {
+    type: 'line',
+    data: dataPosicion,
+    options: {}
+};
+
+const myChartPosicion = new Chart(
+    document.getElementById('posicion'),
+    configPosicion
+);
+
+console.log("Desde Posicion: ", responseService)
---
+++ views/escolaridad/escolaridadchart.php
@@ -0,0 +1,6 @@
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title">Gráfica Escolaridad</h5>
+        <canvas id="escolaridad"></canvas>
+    </div>
+</div>
