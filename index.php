6703c38dd02c0a8969b726e764a452fc18052ca0
Maria Fernanda Velandia <mfvelandiab@outlook.com>
2022-06-20 22:13:24 -0500

    feat(lateralidad): Add lateralidad chart and navbar component

--- index.php
+++ index.php
@@ -4,12 +4,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="./public/css/style.css">
   </head>
-  <body>
+  <body class="bg-light">
+    <?php require 'views/navbar/navbar.php'; ?>
     <div class="container my-3">
-      <h1>Grupo 2</h1>
-    </div>
-    <div class="container my-3">
-      <h2>Charts</h2>
+      <h2 class="display-4">Charts</h2>
       <!-- Inicio primera fila-->
       <div class="row my-3">
         <!-- Inicio primer columna-->
---
+++ views/navbar/navbar.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Grupo 2</a>
+    </div>
+</nav>
